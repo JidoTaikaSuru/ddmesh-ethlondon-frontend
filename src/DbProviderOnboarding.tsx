fb9926d0f756b6a8934c3a57b00ffd2a25321f95
Jeremy N <jkn1092@gmail.com>
2024-03-16 17:22:22 +0000

    Change design list providers

--- src/DbProviderOnboarding.tsx
+++ src/DbProviderOnboarding.tsx
@@ -14,6 +14,127 @@ import { abi as ddmeshMarketAbi } from "./../contracts/DDMeshMarket.sol/DDMeshMa
 
 import { ToastAction } from "@/components/ui/toast";
 import { useToast } from "@/components/ui/use-toast";
+import * as React from "react";
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const data: Provider[] = [
+  {
+    id: "derv1ws0",
+    storagePrice: "$50/mo",
+    name: "One",
+    description: "500gb 2vcore",
+    tvl: "$10 TVL",
+  },
+  {
+    id: "5kma53ae",
+    storagePrice: "$5/mo",
+    name: "Two",
+    description: "0.5gb 0.2",
+    tvl: "$50 TVL",
+  },
+  {
+    id: "bhqecj4p",
+    storagePrice: "$14/mo",
+    name: "Three",
+    description: "200gb serverless",
+    tvl: "$136 TVL",
+  },
+]
+
+export type Provider = {
+  id: string
+  storagePrice: string
+  name: string
+  description: string
+  tvl: string
+}
+
+export const columns: ColumnDef<Provider>[] = [
+  {
+    id: "id",
+    header: "",
+    cell: ({ row }) => (
+        <Jazzicon
+            diameter={60}
+            seed={jsNumberForAddress(
+                "0x1111111111111111111111111111111111111111"
+            )}
+        />
+    ),
+  },
+  {
+    accessorKey: "storagePrice",
+    header: "Storage Price",
+    cell: ({ row }) => (
+        <div className={"flex-col"}>
+          <p className={"text-xl flex"}>
+            <img className={"h-5"} src={UsdcLogo} />
+            <p>{row.getValue("storagePrice")}</p>
+          </p>
+        </div>
+    ),
+  },
+  {
+    accessorKey: "dataProvider",
+    header: "Data Provider",
+    cell: ({ row }) => (
+        <div className={"flex-col align-middle justify-center"}>
+          <img style={{ height: 25 }} src={PostgresLogo} />
+        </div>
+    ),
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("name")}</div>
+    ),
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+    cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("description")}</div>
+    ),
+  },
+  {
+    accessorKey: "tvl",
+    header: "TVL",
+    cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("tvl")}</div>
+    ),
+  },
+  {
+    accessorKey: "button",
+    header: "",
+    cell: ({row}) => (
+        <div>
+          <Button onClick={() => onDeploy()}>Deploy</Button>
+        </div>
+    )
+  }
+]
 
 export const DbProviderOnboarding = () => {
   const navigate = useNavigate();
@@ -25,6 +146,33 @@ export const DbProviderOnboarding = () => {
   const ddmeshMarketAddress = getContracts(chainId)
     .ddmeshMarket as `0x${string}`;
   console.log("ddmeshMarketAddress", ddmeshMarketAddress);
+
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+      []
+  )
+  const [columnVisibility, setColumnVisibility] =
+      React.useState<VisibilityState>({})
+  const [rowSelection, setRowSelection] = React.useState({})
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  })
 
   const {
     data: hash,
@@ -93,73 +241,82 @@ export const DbProviderOnboarding = () => {
 
   return (
     <>
-      <div>
-        <Card className={"p-4 flex items-center leading-4 gap-x-2"}>
-          <Checkbox style={{ height: 30, width: 30 }} />
-          <Jazzicon
-            diameter={60}
-            seed={jsNumberForAddress(
-              "0x1111111111111111111111111111111111111111"
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                        <TableHead
+                            key={header.id}
+                            className="[&:has([role=checkbox])]:pl-3"
+                        >
+                          {header.isPlaceholder
+                              ? null
+                              : flexRender(
+                                  header.column.columnDef.header,
+                                  header.getContext()
+                              )}
+                        </TableHead>
+                    )
+                  })}
+                </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                    <TableRow
+                        key={row.id}
+                        data-state={row.getIsSelected() && "selected"}
+                    >
+                      {row.getVisibleCells().map((cell) => (
+                          <TableCell
+                              key={cell.id}
+                              className="[&:has([role=checkbox])]:pl-3"
+                          >
+                            {flexRender(
+                                cell.column.columnDef.cell,
+                                cell.getContext()
+                            )}
+                          </TableCell>
+                      ))}
+                    </TableRow>
+                ))
+            ) : (
+                <TableRow>
+                  <TableCell
+                      colSpan={columns.length}
+                      className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
             )}
-          />
-          <p className={"text-xl"}>Data Provider 1</p>
-          <div className={"flex-col"}>
-            <p className={"text-sm"}>Rank</p>
-            <p className={"text-xl"}>#1</p>
-          </div>
-          <div className={"flex-col align-middle justify-center"}>
-            <p className={"text-sm"}>Database</p>
-            <img style={{ height: 25 }} src={PostgresLogo} />
-          </div>
-          <div className={"flex-col"}>
-            <p className={"text-sm"}>Storage Available</p>
-            <p className={"text-xl"}>100GB</p>
-          </div>
-          <div className={"flex-col"}>
-            <p className={"text-sm"}>Storage Price</p>
-            <p className={"text-xl flex"}>
-              <img className={"h-5"} src={UsdcLogo} />
-              <p>0.001/min</p>
-            </p>
-          </div>
-          <div>
-            {/* <Button onClick={() => navigate(`/newDbProvider`)}>Deploy</Button> */}
-            <Button onClick={() => onDeploy()}>Deploy</Button>
-          </div>
-        </Card>
+          </TableBody>
+        </Table>
 
-        <Card className={"p-4 flex items-center leading-4 gap-x-2"}>
-          <Checkbox style={{ height: 30, width: 30 }} />
-          <Jazzicon
-            diameter={60}
-            seed={jsNumberForAddress(
-              "0x1111111111111111111111111111111111111111"
-            )}
-          />
-          <p className={"text-xl"}>Data Provider 1</p>
-          <div className={"flex-col"}>
-            <p className={"text-sm"}>Rank</p>
-            <p className={"text-xl"}>#1</p>
+        <div className="flex items-center justify-end space-x-2 pt-4">
+          <div className="flex-1 text-sm text-muted-foreground">
+            <Button
+                variant="outline"
+                size="sm"
+                onClick={() => table.previousPage()}
+                disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+                variant="outline"
+                size="sm"
+                onClick={() => table.nextPage()}
+                disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
           </div>
-          <div className={"flex-col align-middle justify-center"}>
-            <p className={"text-sm"}>Database</p>
-            <img style={{ height: 25 }} src={PostgresLogo} />
-          </div>
-          <div className={"flex-col"}>
-            <p className={"text-sm"}>Storage Available</p>
-            <p className={"text-xl"}>100GB</p>
-          </div>
-          <div className={"flex-col"}>
-            <p className={"text-sm"}>Storage Price</p>
-            <p className={"text-xl flex"}>
-              <img className={"h-5"} src={UsdcLogo} />
-              <p>0.001/min</p>
-            </p>
-          </div>
-          <div>
-            <Button onClick={() => onDeploy()}>Deploy</Button>
-          </div>
-        </Card>
+        </div>
       </div>
     </>
   );
