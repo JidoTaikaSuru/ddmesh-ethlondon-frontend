40f6a3e6297d23ebf69cdded73b882cb9973b381
adoll <20057155+ad0ll@users.noreply.github.com>
2024-03-17 03:12:41 -0400

    Add query dialog

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import path from "path";
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), nodePolyfills({ include: ['fs', 'stream', "process", "net", ] })],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
