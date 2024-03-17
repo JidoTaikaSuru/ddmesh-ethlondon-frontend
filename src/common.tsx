e96c7577eef43f7a3fdba39bb5333c1925ba9d78
vantwoutmaarten <maarten@vantwout.net>
2024-03-17 03:37:40 +0000

    fix: make the db creator form buttons a bit better.

--- src/common.tsx
+++ src/common.tsx
@@ -1,65 +1,87 @@
-import {Button, ButtonProps} from "@/components/ui/button.tsx";
+import { Button, ButtonProps } from "@/components/ui/button.tsx";
 import * as React from "react";
-import {FC} from "react";
+import { FC } from "react";
+import DDMeshLogo from "./assets/ddmesh-logo-fixed.svg";
 
 const ONE_WEEK = 60 * 24 * 7;
 const ONE_MONTH = 60 * 24 * 30;
-export const IconButton: React.FC<{ icon: string, text: string, buttonProps: ButtonProps }> = ({icon, text, buttonProps}) => {
-    return (<Button className="flex items-center w-48 gap-2" {...buttonProps}>
-        <div className="bg-black rounded-full flex justify-center items-center w-6 h-6">
-            <img src={icon} alt={text} className="w-4 h-4"/>
-        </div>
-        <span className="flex-grow text-left">{text}</span>
-    </Button>)
-}
+export const IconButton: React.FC<{
+  icon: string;
+  text: string;
+  buttonProps: ButtonProps;
+}> = ({ icon, text, buttonProps }) => {
+  return (
+    <Button className="flex items-center w-48 gap-2" {...buttonProps}>
+      <div className="bg-black rounded-full flex justify-center items-center w-6 h-6">
+        <img src={icon} alt={text} className="w-4 h-4" />
+      </div>
+      <span className="flex-grow text-left">{text}</span>
+    </Button>
+  );
+};
 
-export const RenderMESHInWeek: FC<{perMinuteFee: number}> = ({perMinuteFee}) => {
-    return <div>
-        <div>Per week</div>
-        <div>{(perMinuteFee * ONE_WEEK).toFixed(2)} MESH</div>
+export const RenderMESHInWeek: FC<{ perMinuteFee: number }> = ({
+  perMinuteFee,
+}) => {
+  return (
+    <div>
+      <div>Per week</div>
+      <div className={"flex justify-between items-center space-x-2 gap-1"}>
+        <img src={DDMeshLogo} style={{ height: 20 }} />
+        {(perMinuteFee * ONE_WEEK).toFixed(2)} DDM
+      </div>
     </div>
-}
+  );
+};
 
-export const RenderMESHInMonth: FC<{perMinuteFee: number}> = ({perMinuteFee}) => {
-    return <div className={"flex-col text-center"}>
-        <div>Per month</div>
-        <div>{(perMinuteFee * ONE_MONTH).toFixed(2)} MESH</div>
+export const RenderMESHInMonth: FC<{ perMinuteFee: number }> = ({
+  perMinuteFee,
+}) => {
+  return (
+    <div className={"flex-col text-center"}>
+      <div>Per month</div>
+
+      <div className={"flex justify-center items-center space-x-2 gap-1"}>
+        <img src={DDMeshLogo} style={{ height: 20 }} />
+        {(perMinuteFee * ONE_MONTH).toFixed(2)} DDM
+      </div>
     </div>
-}
+  );
+};
 
-export const hardcodedDDMToUsdFee = () => 50
+export const hardcodedDDMToUsdFee = () => 50;
 
-export const CenterAlignedHeader: FC<{ header: string }> = ({header}) => (
-    <div className="capitalize text-center">{header}</div>
-)
+export const CenterAlignedHeader: FC<{ header: string }> = ({ header }) => (
+  <div className="capitalize text-center">{header}</div>
+);
 export type Agreement = {
-    id: bigint; // or number, if within JS safe integer range
-    user: string; // Address as a string
-    userBalance: bigint; // or number
-    providerAddress: string; // Address as a string
-    providerId: bigint; // or number
-    providerClaimed: bigint; // or number
-    encConnectionString: string;
-    startTimeStamp: bigint; // or number, timestamp as number is usually safe
-    status: AgreementStatus;
+  id: bigint; // or number, if within JS safe integer range
+  user: string; // Address as a string
+  userBalance: bigint; // or number
+  providerAddress: string; // Address as a string
+  providerId: bigint; // or number
+  providerClaimed: bigint; // or number
+  encConnectionString: string;
+  startTimeStamp: bigint; // or number, timestamp as number is usually safe
+  status: AgreementStatus;
 };
 
 export enum AgreementStatus {
-    NONE,
-    ENTERED,
-    ACTIVE,
-    CLOSED,
-    REVOKED,
-    ERROR,
+  NONE,
+  ENTERED,
+  ACTIVE,
+  CLOSED,
+  REVOKED,
+  ERROR,
 }
 
 export type Provider = {
-    id: bigint;
-    pAddress: string;
-    fee: bigint; // DDM Tokens
-    encApiKey: string;
-    ensName: string;
-    description: string;
-    noOfDbAgreements: bigint;
-    activeAgreements: bigint;
+  id: bigint;
+  pAddress: string;
+  fee: bigint; // DDM Tokens
+  encApiKey: string;
+  ensName: string;
+  description: string;
+  noOfDbAgreements: bigint;
+  activeAgreements: bigint;
 };
