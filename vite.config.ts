010092618effe868724831a15d2be1fff1f9e380
vantwoutmaarten <maarten@vantwout.net>
2024-03-17 08:00:39 +0000

    fix: add the celoAlfajores contracts with the cUSD token as payment method.

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,26 @@
 import path from "path";
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), nodePolyfills({ include: ['fs', 'stream', "process", "net", ] })],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    define: {
-        "process.env": {},
+  },
+  define: {
+    "process.env": {},
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/neon": {
+        target: "https://console.neon.tech/api/v2",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/neon/, ""),
+      },
     },
-    server: {
-        port: 3000,
-        proxy: {
-            '/neon': {
-                target: 'https://console.neon.tech/api/v2',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/neon/, ''),
-            },
-        },
-    },
-
+  },
 });
